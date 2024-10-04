834f4e290767a03e9144cf209da7d5f2c94edbfc
Klaus Gibin <gibin.klaus@gmail.com>
2024-10-04 10:16:24 -0300

    resolução aula03

--- aula03/exercicios.py
+++ aula03/exercicios.py
@@ -4,6 +4,24 @@
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
 
+# qtd = [30,40,40,10,-10]
+# preco = [-10,40,0,10,-10]
+
+
+
+# for i in range(0,len(qtd)):
+#     print(f"'qtd': {qtd[i]}")
+#     print(f"'preco': {preco[i]}")
+
+#     if qtd[i] > 0 and preco[i] > 0 :
+#         print("Dados Valdios")
+#         print("--------------")
+#     else:
+#         print("Dados Inválidos")
+#         print("--------------")
+
+
+
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
@@ -14,6 +32,29 @@
 # com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+# logs = {'1': {'timestamp': '2021-06-23 10:00:00', 'level': 'OK', 'message': 'Sem Erros'},
+# '2': {'timestamp': '2021-06-23 11:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'},
+# '3': {'timestamp': '2021-06-23 12:00:00', 'level': 'ERROR', 'message': 'Falha na Rede'},
+# '4': {'timestamp': '2021-06-23 10:00:00', 'level': 'OK', 'message': 'Sem Erros'}}
+
+# error_logs = {}
+
+# for key, value in logs.items():
+#     if value['level'] == 'ERROR':
+#         error_logs[key] = value
+
+# ## Imprimindo os Logs com Erros
+# print("Logs com nível ERROR")
+# print("--------------------")
+# print()
+# for key, value in error_logs.items():
+#     print(f"Chave: {key}")
+#     print(f"Data: {value['timestamp']}")
+#     print(f"Level: {value['level']}")
+#     print(f"Mensagem: {value['message']}")
+#     print("--------------------")
+#     print()
+
 
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
@@ -21,14 +62,56 @@
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 
+# import re
+
+# inf_user = {'idade':'19','email':'####@gmail.com'}
+
+# idade_valida = False
+# email_valido = False
+
+# # Validação Range de Idade
+# IDADE_MIN = 18
+# IDADE_MAX = 65
+
+# if int(inf_user['idade']) >= IDADE_MIN and int(inf_user['idade']) <= IDADE_MAX:
+#     idade_valida = True
+
+# #Validação E-mail
+# padrao_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+# email_valido = re.match(padrao_email,inf_user['email']) is not None
+
+# if idade_valida and email_valido:
+#     print("Dados de usuário válidos")
+# elif not idade_valida:
+#     print("Idade não está entre 18 e 65 anos")
+# else:    
+#     print("E-mail Inválido")
+
+
+
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
 
+
+
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+# texto = "era uma vez uma casa de campo, esta casa era bem pequena e uma vez caiu."
+
+# palavras = texto.split() # Quebrando o texto pelo espaço, delimitador padrao
+# contagem = {}
+
+# for palavra in palavras:
+#     if palavra in contagem:
+#         contagem[palavra] += 1
+#     else:
+#         contagem[palavra] = 1
+
+# for palavra, count in contagem.items():
+#     print(f"'{palavra}': {count}")
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
